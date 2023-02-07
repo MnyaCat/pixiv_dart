6e8d02c8c0371583f654aa25d787fcbd7a7415f3
MnyaCat <mnnk.1314gmail.com>
2023-02-07 22:05:27 +0900

    Added function to convert Ugoira to GIF

--- lib/pixiv_dart.dart
+++ lib/pixiv_dart.dart
@@ -2,6 +2,7 @@ library pixiv_dart;
 
 export 'src/client.dart';
 export 'src/constants.dart';
+export 'src/convert.dart';
 export 'src/exceptions.dart';
 export 'src/extensions.dart';
 export 'src/model.dart';
---
+++ lib/src/convert.dart
@@ -0,0 +1,62 @@
+import 'dart:typed_data';
+
+import 'package:archive/archive_io.dart';
+import 'package:image/image.dart';
+
+import 'model/ugoira_metadata.dart';
+
+Future<Command> ugoiraToGifCommand(
+  Uint8List ugoiraData,
+  UgoiraMetadata metadata,
+) async {
+  final ugoira = ZipDecoder().decodeBytes(ugoiraData);
+  final command = Command();
+  final frames = <Image>[];
+
+  for (final frame in metadata.frames) {
+    // // 見つからなかったらnullが返される
+    final frameFile = ugoira.findFile(frame.file);
+    // ArchiveFile.contentがdynamicなのでUint8Listに変換する
+    // デコードに問題があるとnullが返される
+    command.decodeJpg(frameFile!.content as Uint8List);
+    final image = await command.getImage();
+    image!.frameDuration = frame.delay ~/ 10;
+    frames.add(image);
+  }
+
+  return command
+    ..addFrames(frames.length, (frameIndex) => frames[frameIndex])
+    ..encodeGif();
+}
+
+Future<Image?> ugoiraToGifImage(
+  Uint8List ugoiraData,
+  UgoiraMetadata metadata,
+) async {
+  final command = await ugoiraToGifCommand(ugoiraData, metadata);
+  return command.getImage();
+}
+
+Future<Image?> ugoiraToGifImageThread(
+  Uint8List ugoiraData,
+  UgoiraMetadata metadata,
+) async {
+  final command = await ugoiraToGifCommand(ugoiraData, metadata);
+  return command.getImageThread();
+}
+
+Future<Uint8List?> ugoiraToGifBytes(
+  Uint8List ugoiraData,
+  UgoiraMetadata metadata,
+) async {
+  final command = await ugoiraToGifCommand(ugoiraData, metadata);
+  return command.getBytes();
+}
+
+Future<Uint8List?> ugoiraToGifBytesThread(
+  Uint8List ugoiraData,
+  UgoiraMetadata metadata,
+) async {
+  final command = await ugoiraToGifCommand(ugoiraData, metadata);
+  return command.getBytesThread();
+}
