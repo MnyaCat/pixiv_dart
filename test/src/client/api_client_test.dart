1acaf728e9d762ba7de816f6ac732fc154c33614
MnyaCat <mnnk.1314@gmail.com>
2023-06-21 21:25:34 +0900

    Novel APIのテストコードをMockClientに対応

--- test/src/client/api_client_test.dart
+++ test/src/client/api_client_test.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:math';
 
 import 'package:pixiv_dart/pixiv_dart.dart';
 import 'package:test/test.dart';
@@ -42,9 +41,8 @@ void main() async {
       expect(bookmark.isBookmarked, isTrue);
       expect(bookmark.restrict, Restrict.public);
       expect(
-        bookmark.getRegisteredTags(),
-        ['刻晴', '刻晴(原神)', '原神']
-            .map((name) => BookmarkTag(name: name, isRegistered: true)),
+        bookmark.getRegisteredTags().map((tag) => tag.name),
+        ['刻晴', '刻晴(原神)', '原神'],
       );
     });
     test('fetchIllustDetail test', () async {
@@ -52,8 +50,8 @@ void main() async {
       expect(illust.id, illustId);
       expect(illust.title, '刻晴');
       expect(
-        illust.tags,
-        ['原神', '刻晴', '原神10000users入り', '刻晴(原神)'].map((name) => Tag(name: name)),
+        illust.tags.map((tag) => tag.name),
+        ['原神', '刻晴', '原神10000users入り', '刻晴(原神)'],
       );
     });
 
@@ -116,163 +114,69 @@ void main() async {
 
   group('Novel api', () {
     const novelId = 11674134;
-    group('addNovelBookmark test', () {
-      test('add public test', () async {
-        final randomTag = Random().nextInt(10000).toString();
-        await apiClient!.addNovelBookmark(novelId, tags: [randomTag]);
-        final bookmarkedDetail =
-            await apiClient!.fetchNovelBookmarkedDetail(novelId);
-        expect(bookmarkedDetail.isBookmarked, true);
-        expect(
-          bookmarkedDetail.tags
-              .contains(BookmarkTag(name: randomTag, isRegistered: true)),
-          true,
-        );
-        await apiClient!.deleteNovelBookmark(novelId);
-      });
-      test('add private test', () async {
-        final randomTag = Random().nextInt(10000).toString();
-        await apiClient!.addNovelBookmark(
-          novelId,
-          restrict: Restrict.private,
-          tags: [randomTag],
-        );
-        final bookmarkedDetail =
-            await apiClient!.fetchNovelBookmarkedDetail(novelId);
-        expect(bookmarkedDetail.isBookmarked, true);
-        expect(bookmarkedDetail.restrict, Restrict.private);
-        expect(
-          bookmarkedDetail.tags
-              .contains(BookmarkTag(name: randomTag, isRegistered: true)),
-          true,
-        );
-        await apiClient!.deleteNovelBookmark(novelId);
-      });
+    test('addNovelBookmark test', () async {
+      await apiClient!.addNovelBookmark(novelId);
     });
     test('deleteNovelBookmark test', () async {
-      await apiClient!.addNovelBookmark(novelId);
       await apiClient!.deleteNovelBookmark(novelId);
-      final bookmarkDetail =
-          await apiClient!.fetchNovelBookmarkedDetail(novelId);
-      expect(
-        bookmarkDetail.isBookmarked,
-        false,
-      );
     });
     test('fetchNovelBookmarkedDetail test', () async {
-      final bookmarkedDetail =
-          await apiClient!.fetchNovelBookmarkedDetail(17137089);
-      final json = await jsonFileDecode(
-        'test/response/fetch_novel_bookmarked_detail.json',
-      );
-      final matcherBookmarkDetail = BookmarkDetail.fromJson(json);
-      expect(bookmarkedDetail.isBookmarked, matcherBookmarkDetail.isBookmarked);
+      final bookmark = await apiClient!.fetchNovelBookmarkedDetail(novelId);
+      expect(bookmark.isBookmarked, isTrue);
+      expect(bookmark.restrict, Restrict.public);
       expect(
-        bookmarkedDetail.tags.where((tag) => tag.isRegistered),
-        matcherBookmarkDetail.tags.where((tag) => tag.isRegistered),
+        bookmark.getRegisteredTags().map((tag) => tag.name),
+        ['百合', '100点でも足りない', '大人の階段', '創作百合', '社会人百合', '百合文芸4'],
       );
-      expect(bookmarkedDetail.restrict, matcherBookmarkDetail.restrict);
     });
     test('fetchNovelDetail test', () async {
       final novel = await apiClient!.fetchNovelDetail(novelId);
-      final json =
-          await jsonFileDecode('test/response/fetch_novel_detail.json');
-      final matcherNovel = Novel.fromJson(json);
-      expect(novel.id, matcherNovel.id);
-      expect(novel.title, matcherNovel.title);
-      expect(novel.createDate, matcherNovel.createDate);
+      expect(novel.id, novelId);
+      expect(novel.title, '裸足で踊る');
+      expect(
+        novel.tags.map((tag) => tag.name),
+        [
+          '創作百合',
+          '百合',
+          '社会人百合',
+          '大人の階段',
+          '100点でも足りない',
+          '百合小説50users入り',
+          '百合小説100users入り',
+          '百合文芸4'
+        ],
+      );
     });
-    group('fetchLatestNovels test', () {
-      test('fetch latest illust', () async {
-        await apiClient!.fetchLatestNovels();
-      });
-      test('fetch latest illust 31~60', () async {
-        final novels = await apiClient!.fetchLatestNovels();
-        final lastIndex = novels.novels.length;
-        final maxNovelId = novels.novels[lastIndex - 1].id;
-        await apiClient!.fetchLatestNovels(maxNovelId: maxNovelId);
-      });
+    test('fetchLatestNovels test', () async {
+      final novels = await apiClient!.fetchLatestNovels();
+      expect(novels.novels.length, 30);
+      expect(novels.novels[0].id, 19977212);
     });
-    group('fetchLatestNovelsByFollowing test', () {
-      test('fetch all follow', () async {
-        await apiClient!.fetchLatestNovelsByFollowing();
-      });
-      test('fetch public follow', () async {
-        await apiClient!
-            .fetchLatestNovelsByFollowing(restrict: FollowingRestrict.public);
-      });
-      test('fetch private follow', () async {
-        await apiClient!
-            .fetchLatestNovelsByFollowing(restrict: FollowingRestrict.private);
-      });
-      test('fetch all follow 31~60', () async {
-        await apiClient!.fetchLatestNovelsByFollowing(offset: 30);
-      });
+    test('fetchLatestNovelsByFollowing test', () async {
+      final novels = await apiClient!.fetchLatestNovelsByFollowing();
+      expect(novels.novels.length, 30);
+      expect(novels.novels[0].id, 19971288);
     });
     // mypixivがいないのでv1/novel/followでテスト
-    group('fetchLatestNovelsByMyPixiv test', () {
-      test('fetch latest novel mypixiv', () async {
-        await apiClient!.fetchLatestNovelsByMyPixiv();
-      });
-      test('fetch latest novel mypixiv 31~60', () async {
-        await apiClient!.fetchLatestNovelsByMyPixiv(offset: 30);
-      });
+    test('fetchLatestNovelsByMyPixiv test', () async {
+      final novels = await apiClient!.fetchLatestNovelsByMyPixiv();
+      expect(novels.novels.length, 30);
+      expect(novels.novels[0].id, 19971288);
     });
-    group('fetchNovelRanking test', () {
-      test('fetch daily ranking test', () async {
-        await apiClient!.fetchNovelRanking();
-      });
-      test('fetch daily ranking #31~60', () async {
-        await apiClient!.fetchNovelRanking(offset: 30);
-      });
-      test('fetch daily ranking on 01/01/2023 test', () async {
-        final novels = await apiClient!.fetchNovelRanking(date: DateTime(2023));
-        final json = await jsonFileDecode(
-          'test/response/fetch_novel_ranking_2023_01_01.json',
-        );
-        final matcherNovels = Novels.fromJson(json);
-        expect(novels.novels[0].id, matcherNovels.novels[0].id);
-        expect(novels.novels[0].title, matcherNovels.novels[0].title);
-      });
+    test('fetchNovelRanking test', () async {
+      final novels = await apiClient!.fetchNovelRanking();
+      expect(novels.novels.length, 30);
+      expect(novels.novels[0].id, 19753616);
     });
-    group('fetchRecommendedNovels test', () {
-      test('fetch recommended novel', () async {
-        await apiClient!.fetchRecommendedNovels();
-      });
-      test('fetch recommended novel 30~60', () async {
-        final novels = await apiClient!.fetchRecommendedNovels();
-        final queries = Uri.parse(novels.nextUrl!).queryParameters;
-        final alreadyRecommended = queries['already_recommended']!
-            .split(',')
-            .map<int>(int.parse)
-            .toList();
-        await apiClient!.fetchRecommendedNovels(
-          offset: int.parse(queries['offset']!),
-          maxBookmarIdForRecommended:
-              int.parse(queries['max_bookmark_id_for_recommend']!),
-          alreadyRecommended: alreadyRecommended,
-        );
-      });
+    test('fetchRecommendedNovels test', () async {
+      final novels = await apiClient!.fetchRecommendedNovels();
+      expect(novels.novels.length, 33);
+      expect(novels.novels[0].id, 14103227);
     });
-    group('fetchRelatedNovels test', () {
-      test('fetch related novel', () async {
-        await apiClient!.fetchRelatedNovels(novelId);
-      });
-      test('fetch related novel 31~60', () async {
-        final novels = await apiClient!.fetchRelatedNovels(novelId);
-        final queries = Uri.parse(novels.nextUrl!).queryParameters;
-        final viewed = <int>[];
-        queries.forEach((key, value) {
-          if (key.contains('viewed')) {
-            viewed.add(int.parse(value));
-          }
-        });
-        await apiClient!.fetchRelatedNovels(
-          novelId,
-          seedNovelIds: [int.parse(queries['seed_novel_ids[0]']!)],
-          viewed: viewed,
-        );
-      });
+    test('fetchRelatedNovels novel', () async {
+      final novels = await apiClient!.fetchRelatedNovels(novelId);
+      expect(novels.novels.length, 30);
+      expect(novels.novels[0].id, 18938078);
     });
   });
 
